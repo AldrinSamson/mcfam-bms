bfac91fc6e0dcf92ea7dd9c54ea4872edf3b4951
Mark Samuel Nicasio <marksamuel.nicasio@gmail.com>
2020-03-08 20:36:49 +0800

    file model

---
+++ src/app/shared/models/file.model.ts
@@ -0,0 +1,9 @@
+export class FileModel {
+    id?: string;
+    fileProperties: any;
+    uidUploaded:string;
+    section: 'BMS';
+    fileName: string;
+    category: string;
+    photoURL: string;
+}
